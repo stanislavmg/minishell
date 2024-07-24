0f5c65287dede07c2da7eed24eed7bd2aa7fc698
success <raidloz@icloud.com>
2024-07-24 09:19:46 +0400

    FIX | delete one method fixed

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,7 +6,7 @@
 /*   By: amikhush <<marvin@42.fr>>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 10:30:45 by amikhush          #+#    #+#             */
-/*   Updated: 2024/07/23 11:01:55 by amikhush         ###   ########.fr       */
+/*   Updated: 2024/07/24 08:24:30 by amikhush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,18 @@ typedef struct s_env
 int		handle_cd(char **args, t_env *env, int *fd);
 int		handle_env(char **args, t_env *env, int *fd);
 int		handle_pwd(char **args, t_env *env, int *fd);
+int		handle_export(char **args, t_env *env, int *fd);
+int		handele_echo(char **args, t_env *env, int *fd);
+int		handle_unset(char **args, t_env *env, int *fd);
+int		handle_exit(char **args, t_env *env, int *fd);
 char	*get_env(char *key, t_env *env);
 int		set_env(t_env *env, char *key, char *value);
 t_env	*list_new(char *key, char *value);
 void	list_add(t_env **lst, t_env *new);
 t_env	*list_search(t_env *lst, char *key);
-void	list_delete_one(t_env *lst, char *key);
+void	list_delete_one(t_env **lst, char *key);
 void	list_delete(t_env **lst);
+int		ft_list_size(t_env *lst);
 char	**ft_first_split(char const *s, char c);
 int		ft_strcmp(const char *s1, const char *s2);
 
